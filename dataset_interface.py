466dc9d7b6743584f5a790db1681360b99e3cf8e
Wind-Wing <windwing.me@gmail.com>
2023-11-04 19:15:23 +0800

    A

--- dataset_interface.py
+++ dataset_interface.py
@@ -165,12 +165,14 @@ if __name__ == "__main__":
     pool = multiprocessing.Pool(12)
 
     _dir = "/media/zfchen/048c6a59-054e-45ae-aa0e-1e437096b891/zeta/audio_set"
-    _f = [i.strip(".wav") for i in os.listdir(_dir) if i.endswith(".wav")]
+    _f = [i.removesuffix(".wav") for i in os.listdir(_dir) if i.endswith(".wav")]
+    print(len(_f))
+    print(len(list(set(_f) & set(audio_set.audio_ids))))
     for i in _f:
         if i not in audio_set.audio_ids:
             os.system(f"rm {os.path.join(_dir, i)}.wav")
 
-    _f = [i.strip(".wav") for i in os.listdir(_dir) if i.endswith(".wav")]
+    _f = [i.removesuffix(".wav") for i in os.listdir(_dir) if i.endswith(".wav")]
     print(len(_f))
     # r = list(tqdm(pool.imap(audio_set.check_length, _f), total=len(_f)))
     # r = list(tqdm(pool.imap(audio_set.check_length, audio_set.audio_ids), total=len(audio_set.audio_ids)))
